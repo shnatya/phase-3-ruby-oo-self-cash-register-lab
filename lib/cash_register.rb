7d563dda07a5b74f47acafbbc3f6c51b13479ea8
Anastasia Shatokhina <shnatya@gmail.com>
2022-01-26 18:06:33 -0800

    commit tree 3b0bd4d60d3330cc2fbf9d19186f52e421ab88f0

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,38 @@
+require "pry"
+class CashRegister
+    attr_accessor :total, :last, :items_arr
+    attr_reader :discount
 
+    def initialize(discount=0)
+        @total = 0
+        @discount = discount
+        @last = 0
+        @items_arr = []
+    end
+    
+    def add_item(title, price, quantity=1)
+        self.total += price*quantity.to_i
+        @last= price*quantity.to_i
+        quantity.times {self.items_arr << title} #{@items_arr << title}
+    end
+
+    def apply_discount
+        if self.discount > 0
+            self.total -= self.total*self.discount/100.to_i
+            "After the discount, the total comes to $#{self.total.to_i}."
+        else 
+             "There is no discount to apply."
+        end
+    end
+
+    def void_last_transaction
+        self.total= (total - last) # total and last - methods and they return @total and @last?
+          #self.total= (@total - @last) #instance  variables?
+        #self.total -= @last self.total= self.total - @last
+        #@total -= @last 
+    end
+
+    def items
+        @items_arr
+    end
+end
